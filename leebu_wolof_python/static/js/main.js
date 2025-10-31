60a6fb6f96f88caf7e47d9d813527fa3e22b864f
pafndio <62841045+pafndio@users.noreply.github.com>
2025-10-31 00:16:52 +0000

    Delete leebu_wolof_python directory

--- leebu_wolof_python/static/js/main.js
+++
@@ -1,6 +0,0 @@
-// Fichier JavaScript principal pour Leebu Wolof
-
-// Initialisation au chargement de la page
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Leebu Wolof chargé avec succès');
-});
